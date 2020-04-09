98a09a18b0dde224d187882b0c47dbb4e09c57d1
Tyler McBride <tymcbrid@gmail.com>
2020-04-08 17:12:23 -0700

    V1

---
+++ script.js
@@ -0,0 +1,90 @@
+var currentCity = document.querySelector("#currentCity");
+var currentTemp = document.querySelector("#currentTemp");
+var currentHumidity = document.querySelector("#currentHumidity");
+var currentWind = document.querySelector("#currentWind");
+var currentUV = document.querySelector("#currentUV");
+var searchBtn = document.querySelector("#searchBtn");
+var searchItem = document.querySelector("#searchItem");
+var cityList1 = document.querySelector("#cityList1");
+var cityList2 = document.querySelector("#cityList2");
+var cityList3 = document.querySelector("#cityList3");
+var cityList4 = document.querySelector("#cityList4");
+var cityList5 = document.querySelector("#cityList5");
+var cityList6 = document.querySelector("#cityList6");
+var cityList7 = document.querySelector("#cityList7");
+var cityList8 = document.querySelector("#cityList8");
+
+$("#searchBtn").on("click", function(event) {
+    event.preventDefault();
+    var city = $("#searchItem").val();
+    var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&APPID=a89a05b4a0df6c1694d5f08466c85383"
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function(response){
+        console.log(response);
+        var City = response.name;
+        console.log(City);
+        var Kelvin = (response.main.temp);
+        var Farenheit = Math.round(((Kelvin - 273.15) * 9/5 + 32)*10)/10;
+        var Humidity = (response.main.humidity);
+        var Wind = (response.wind.speed);
+        currentTemp.textContent = "Temperature: " + Farenheit + "°";
+        currentCity.textContent = City + " " + "(" + currentDate + ")";
+        currentHumidity.textContent = "Humidity: " + Humidity + "%";
+        currentWind.textContent = "Wind Speed: " + Wind + " MPH";
+        updateList(City);
+    })
+})
+
+function updateList(City){
+cityList8.textContent = cityList7.textContent;
+cityList7.textContent = cityList6.textContent;
+cityList6.textContent = cityList5.textContent;
+cityList5.textContent = cityList4.textContent;
+cityList4.textContent = cityList3.textContent;
+cityList3.textContent = cityList2.textContent;
+cityList2.textContent = cityList1.textContent;
+cityList1.textContent = City
+}
+
+
+function todaysDate(){
+    var today = moment().format("MM/DD/YY");
+    currentDate = today;
+}
+todaysDate();
+
+function getLocation() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(showPosition);
+    } 
+    else {
+      alert("Geolocation is not supported by this browser.");
+    }
+  }
+  function showPosition(position) {
+    console.log(position);
+    var lat = Math.round(position.coords.latitude * 10)/10;
+    var lon = Math.round(position.coords.longitude * 10)/10;
+    console.log("Your coordinates are Latitude: " + lat + " Longitude " + lon);
+    weatherDisplay(lat, lon);
+  }
+getLocation();
+
+function weatherDisplay(lat, lon) {
+    var queryURL = 'http://api.openweathermap.org/data/2.5/find?lat='+ lat + '&lon=' +  lon + '&APPID=79bb7dc0e8f07f6ebe01166410e6e392'
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function(response){
+        var City = response.list[0].name;
+        var Kelvin = (response.list[0].main.temp);
+        var Farenheit = Math.round(((Kelvin - 273.15) * 9/5 + 32)*10)/10;
+        var Humidity = (response.list[0].main.humidity);
+        var Wind = (response.list[0].wind.speed);
+        currentTemp.textContent = "Temperature: " + Farenheit + "°";
+        currentCity.textContent = City + ", WA " + "(" + currentDate + ")";
+        currentHumidity.textContent = "Humidity: " + Humidity + "%";
+        currentWind.textContent = "Wind Speed: " + Wind + " MPH";
+    })}
